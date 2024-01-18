1f090d5fd9702a5ac873f58c760ccf3465497efc
Suyeon <syhhwang1231@gmail.com>
2024-01-19 02:12:55 +0900

    Create BeRealCloneCoding

---
+++ BeRealCloneCoding/BeRealCloneCoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeRealCloneCoding
+//
+//  Created by Suyeon Hwang on 1/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
